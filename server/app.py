38fb668c441ffc03f855ce5dc051853233c47276
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-06-16 23:16:35 +0300

    upgraded app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,12 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
 from server import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
